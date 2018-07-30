872882c817662d9cab389eb149325bd41e2e0823
Kelvin DeCosta <kelvin.p.decosta@gmail.com>
2018-07-31 01:28:04 +0300

    [program] Define resize function

--- converter.py
+++ converter.py
@@ -1,7 +1,19 @@
 import os
-import cv2
+import cv2 as cv
 
 
+# Open file as grayscale image
 def gray_scale(path):
-    image = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+    image = cv.imread(path, cv.IMREAD_GRAYSCALE)
     return image
+
+
+# Resize image
+def resize(image, scale):
+    h, w = image.shape
+
+    # Scale width further by 1.75 to normalize aspect ratio of a text character
+    h = int(round(h * scale))
+    w = int(round(w * scale * 1.75))
+
+    return cv.resize(image, (w, h))
