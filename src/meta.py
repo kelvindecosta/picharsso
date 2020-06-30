f1080aef4fef231942f57af8b761fd9ede322bc5
Kelvin DeCosta <decostakelvin@gmail.com>
2020-07-01 01:23:34 +0300

    [src/meta] Add metadata module

---
+++ src/meta.py
@@ -0,0 +1,23 @@
+"""This module defines variables for the package metadata."""
+
+NAME: str = "picharsso"
+"""The package name."""
+
+DESCRIPTION: str = "A utility for converting images to text art."
+"""The package description."""
+
+VERSION: str = "1.21"
+"""The current package version.
+"""
+
+REPO_URL: str = f"https://github.com/kelvindecosta/{NAME}"
+"""The URL of the package source code."""
+
+DOCS_URL: str = f"https://kelvindecosta.github.io/{NAME}"
+"""The URL of the package documentation."""
+
+AUTHOR: str = "Kelvin DeCosta"
+"""The package author."""
+
+LICENSE: str = "MIT"
+"""The package license."""
