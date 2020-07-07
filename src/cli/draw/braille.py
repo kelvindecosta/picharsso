0eb13aab9c33072321c52651d30aa721c6fbc343
Kelvin DeCosta <decostakelvin@gmail.com>
2020-07-08 01:52:33 +0300

    [src/cli/draw/braille] Add draw braille command

---
+++ src/cli/draw/braille.py
@@ -0,0 +1,23 @@
+import click
+
+from ...draw import new_drawer
+from ...draw.braille import DEFAULT_THRESHOLD
+
+
+@click.command("braille", options_metavar="[options]")
+@click.option(
+    "-t",
+    "--threshold",
+    type=click.IntRange(0, 255),
+    help="Threshold pixel luminance (from grayscale).",
+    default=DEFAULT_THRESHOLD,
+    show_default=True,
+)
+@click.pass_context
+def draw_braille(context, threshold):
+    """Use the Braille style."""
+
+    image = context.obj.pop("image")
+
+    drawer = new_drawer("braille", threshold=threshold, **context.obj)
+    print(drawer(image))
